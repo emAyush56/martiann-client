e2b6ffcaff3bc0cc8ff34772e33951060b15ffcd
Ayush Saha <searchme.ayush@gmail.com>
2023-05-17 01:29:07 +0530

    highlighted the profile button in the main app page

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -2,9 +2,14 @@ import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
-    <div className="sticky top-0 h-screen w-full border-r border-r-gray-300">
+    <div className="sticky top-0 h-screen w-full border-r border-r-gray-300 px-4">
       <h1>Sidebar</h1>
-      <Link to="/edit-profile">Profile</Link>
+      <Link
+        to="/edit-profile"
+        className="mt-3 block w-fit rounded-2xl bg-theme-purple px-4 py-1 text-white transition-all hover:bg-purple-400"
+      >
+        Profile - /edit-profile
+      </Link>
     </div>
   );
 }
